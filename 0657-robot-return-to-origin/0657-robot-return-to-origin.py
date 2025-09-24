1aaccfa6c35b1101a5ced733341170e8236675a7
Faraz Ali Bukhari Syed <farazsyed3311@gmail.com>
2025-09-24 15:49:10 +0200

    Time: 3 ms (84.91%), Space: 12.7 MB (50.97%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -4,20 +4,5 @@ class Solution(object):
         :type moves: str
         :rtype: bool
         """
-        count_1=0
-        count_2=0
-        for i in range(len(moves)):
-            if moves[i]=="U":
-                count_1+=1
-            elif moves[i]=="D":
-                count_1-=1
-            elif moves[i]=="L":
-                count_2-=1
-            else:
-                count_2+=1
-        if count_1==0 and count_2==0:
-            return True
-        else:
-            return False
-            
+        return moves.count('L')==moves.count('R') and moves.count('U')==moves.count('D')
             
