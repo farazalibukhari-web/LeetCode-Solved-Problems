a967d592631a25e0156dec498c6253ae70138489
Faraz Ali Bukhari Syed <farazsyed3311@gmail.com>
2025-09-28 14:19:38 +0200

    Time: 4 ms (18.28%), Space: 12.4 MB (92.16%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -4,16 +4,13 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        word_1=list("".join(strs[0]))
-        for i in range(1,len(strs)):
-            new_list=[]
-            word_2=list("".join(strs[i]))
-            for j in range(min(len(word_1),len(word_2))):
-                if word_1[j]==word_2[j]:
-                    new_list.append(word_1[j])              
-                else:
-                    break
-            word_1=new_list[::]
-            
-        return "".join(word_1)
+        prefix = strs[0]
+        for word in strs[1:]:
+            j = 0
+            while j < len(prefix) and j < len(word) and prefix[j] == word[j]:
+                j += 1
+            prefix = prefix[:j]
+            if not prefix:   # no common prefix, break early
+                break
+        return prefix
                 
