e7014639cd04976ec0c7249517698014470fa864
Faraz Ali Bukhari Syed <farazsyed3311@gmail.com>
2025-09-30 00:58:53 +0200

    Time: 14 ms (73.98%), Space: 12.4 MB (84.56%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -5,12 +5,16 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        new_string=""
-        large_string=max(word1, word2, key=len)
-        small_string=min(word1, word2, key=len)
-        for i in range(len(large_string)):
-            if i<len(small_string):
-                new_string+=word1[i]+word2[i]
-            else:
-                new_string+=large_string[i]
-        return new_string
+        merge_string = []
+        i,j= 0,0
+        while i < len(word1) and j < len(word2):
+            merge_string.append(word1[i])
+            merge_string.append(word2[j])
+            i +=1
+            j += 1
+        if i < len(word1):
+            merge_string.append(word1[i:])
+        if j < len(word2):
+            merge_string.append(word2[j:])
+
+        return "".join(merge_string)
