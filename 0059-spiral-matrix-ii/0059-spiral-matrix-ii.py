99e497c82f635c14ac827c4615c9e726e4428baa
Faraz Ali Bukhari Syed <farazsyed3311@gmail.com>
2025-10-02 16:36:14 +0200

    Time: 0 ms (100%), Space: 12.3 MB (99.15%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -5,29 +5,13 @@ class Solution(object):
         :rtype: List[List[int]]
         """
 
-        matrix = [[0]*n for _ in range(n)]
-        row_start,row_end=0,len(matrix)-1
-        col_start,col_end=0,len(matrix[0])-1
-        num=0
-        while row_start<=row_end and col_start<=col_end:
-            for col in range(col_start,col_end+1,1):
-                num+=1
-                matrix[row_start][col]=(num)
-            row_start+=1
-            for row in range(row_start,row_end+1,1):
-                num+=1
-                matrix[row][col_end]=(num)
-            col_end-=1
-            if row_start <= row_end:
-                for col in range(col_end,col_start-1,-1):
-                    num+=1
-                    matrix[row_end][col]=(num)
-                row_end-=1
-            if col_start<=col_end:
-                for row in range(row_end,row_start-1,-1):
-                    num+=1
-                    matrix[row][col_start]=(num)
-                col_start+=1
-        return matrix
+        res, i, val =[[0]*n for _ in range(n)], 0, 1
+        while val <= n*n:
+            for r in range(i, n - i): res[i][r] = val; val += 1
+            for r in range(i + 1, n - i): res[r][n - i - 1] = val; val += 1
+            for r in range(n - i - 2, i - 1, -1): res[n - i - 1][r] = val; val += 1
+            for r in range(n - i - 2, i, -1): res[r][i] = val; val += 1
+            i += 1
+        return res
 
         
