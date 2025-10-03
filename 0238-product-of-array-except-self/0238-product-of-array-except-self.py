37b22d2b4acb0707644e46d366893bc4838e74e0
Faraz Ali Bukhari Syed <farazsyed3311@gmail.com>
2025-10-03 20:32:15 +0200

    Time: 34 ms (46.91%), Space: 20.2 MB (85.18%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,21 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        answer=[]
-        last_nums=[1]
-        next_nums=[1]
-
+        answer=[1]*len(nums)
         for i in range(1,len(nums)):
-            last_nums.append(last_nums[i-1]*nums[i-1])
-        
-        nums=nums[::-1]
-        for j in range(1,len(nums)):
-            next_nums.append(next_nums[j-1]*nums[j-1])
-        next_nums=next_nums[::-1]
-        
-        for a in range(len(nums)):
-            answer.append(last_nums[a]*next_nums[a])
-        
-        return answer
+            answer[i]=answer[i-1]*nums[i-1]
+        next_num=1
+        for j in range(len(nums)-1,-1,-1):
+                answer[j]=answer[j]*next_num
+                next_num=next_num*nums[j]
+        return answer  
             
-         
