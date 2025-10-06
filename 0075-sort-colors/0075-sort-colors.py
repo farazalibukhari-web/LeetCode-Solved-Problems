684cbc5b8daa378c0f41f88fc032c52974f9c8e5
Faraz Ali Bukhari Syed <farazsyed3311@gmail.com>
2025-10-07 01:11:15 +0200

    Time: 0 ms (100%), Space: 12.5 MB (52.47%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,18 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        left=0
-        mid=0
-        right=len(nums)-1
-        
-        while mid<=right:
-            if nums[mid]==0:
-                nums[mid],nums[left]=nums[left],nums[mid]
-                mid+=1
-                left+=1
-            elif nums[mid]==1:
-                mid+=1
-            elif nums[mid]==2:
-                nums[mid],nums[right]=nums[right],nums[mid]
-                right-=1
-        return nums
+        red = white = blue = 0
+        for num in nums:
+            if num == 0:
+                red += 1
+            elif num == 1:
+                white += 1
+            else:
+                blue += 1
+        count = 0
+        for _ in range(red):
+            nums[count] = 0
+            count += 1
+        for _ in range(white):
+            nums[count] = 1
+            count += 1
+        for _ in range(blue):
+            nums[count] = 2
+            count += 1
