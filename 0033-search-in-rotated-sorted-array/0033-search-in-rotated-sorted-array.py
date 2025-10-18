5a8e704a0d42c48004b8d84f640021594876ba2b
Faraz Ali Bukhari Syed <farazsyed3311@gmail.com>
2025-10-18 10:57:37 +0200

    Time: 0 ms (100%), Space: 12.7 MB (42.67%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -6,38 +6,20 @@ class Solution(object):
         :rtype: int
         """
     
-        if not nums or len(nums)==1 and nums[0]!=target:
-            return -1
-
-        # Find pivot: index of the smallest element
-        l, r = 0, len(nums) - 1
-        while l < r:
-            mid = (l + r) // 2
-            if nums[mid] > nums[r]:
-                # pivot is to the right
-                l = mid + 1
-            else:
-                # pivot is at mid or to the left
-                r = mid
-        pivot = l  # smallest element index
-
-        # Decide which half to binary-search
-        if target >= nums[pivot] and target <= nums[len(nums) - 1]:
-            l, r = pivot, len(nums) - 1
-        else:
-            l, r = 0, pivot - 1
-
-        # Standard binary search in chosen half
-        while l <= r:
-            mid = (l + r) // 2
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
-                l = mid + 1
+            if nums[mid] > nums[-1]:
+                if nums[0] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
             else:
-                r = mid - 1
-
+                if nums[mid] < target <= nums[-1]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
         return -1
-
-
 
