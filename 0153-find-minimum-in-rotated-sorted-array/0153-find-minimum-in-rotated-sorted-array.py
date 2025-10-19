fecedeed63e4bf526e8aa2126741cfd272c96280
Faraz Ali Bukhari Syed <farazsyed3311@gmail.com>
2025-10-19 14:57:58 +0200

    Time: 0 ms (100%), Space: 12.5 MB (91.69%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -4,6 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        if len(nums) == 1:
+            return nums[0]
+        
         l,r=0,len(nums)-1
         while l<r:
             mid=(l+r)//2
