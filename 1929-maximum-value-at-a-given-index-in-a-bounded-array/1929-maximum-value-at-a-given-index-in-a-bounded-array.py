e05ea0e48f5e1b11b3fec50e7d4ea31743a6682c
Faraz Ali Bukhari Syed <farazsyed3311@gmail.com>
2025-10-23 14:51:50 +0200

    Time: 0 ms (100%), Space: 12.4 MB (75.86%) - LeetHub

--- 1929-maximum-value-at-a-given-index-in-a-bounded-array/1929-maximum-value-at-a-given-index-in-a-bounded-array.py
+++ 1929-maximum-value-at-a-given-index-in-a-bounded-array/1929-maximum-value-at-a-given-index-in-a-bounded-array.py
@@ -6,31 +6,30 @@ class Solution(object):
         :type maxSum: int
         :rtype: int
         """
+
+        def side_sum(peak, length):
+
+            # number of values before reaching 1 on that side
+            dec_steps = peak - 1
+            if dec_steps >= length:
+                return length * (2 * peak - length - 1) // 2
+            else:
+                return (dec_steps * (dec_steps + 1)) // 2 + (length - dec_steps)
+
         low, high = 1, maxSum
         ans = 1
 
         while low <= high:
-            mid = (low + high) // 2
+            mid = (low + high) // 2  # candidate peak value
 
-            # Total sum = left side + middle + right side
-            if mid - 1 >= index:
-                left = index * mid - (index * (index + 1)) // 2
-            else:
-                left = (mid * (mid - 1)) // 2 + (index - (mid - 1))
-
-            right_length = n - index - 1
-            
-            if mid - 1 >= right_length:
-                right = right_length * mid - (right_length * (right_length + 1)) // 2
-            else:
-                right = (mid * (mid - 1)) // 2 + (right_length - (mid - 1))
-
+            left = side_sum(mid, index)                # sum of left side (index elements)
+            right = side_sum(mid, n - index - 1)       # sum of right side
             total = left + mid + right
 
             if total <= maxSum:
-                ans = mid        # mid fits → try higher
+                ans = mid
                 low = mid + 1
             else:
-                high = mid - 1   # too big → go lower
+                high = mid - 1
 
         return ans
