82c44a83ad6c9128a3e4de4fccc4aae7433bba24
Faraz Ali Bukhari Syed <farazsyed3311@gmail.com>
2025-10-26 14:19:22 +0100

    Time: 432 ms (18.53%), Space: 13.5 MB (46.59%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+        import math
+        left = 1
+        right = max(piles)
+        k = right
+
+        while left <= right:
+            mid = (left + right) // 2  #mid=Speed of Koko eating Banana
+            hours = 0
+
+            for p in piles:
+                time = math.ceil(p / float(mid))  #Time taken by koko to finish the piles
+                hours += time
+                
+            if hours <= h:
+                k = mid
+                right = mid - 1   
+            else:
+                left = mid + 1    
+
+        return k
+
