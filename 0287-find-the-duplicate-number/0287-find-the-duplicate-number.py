32ccac5912558463c15f0dcdec2864e4c208e8ed
Faraz Ali Bukhari Syed <farazsyed3311@gmail.com>
2025-10-30 17:36:57 +0100

    Time: 23 ms (96.34%), Space: 26.4 MB (34.64%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -4,15 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        l, r = 1, len(nums) - 1
-        while l < r:
-            mid = (l + r) // 2
-            count = 0
-            for num in nums:
-                if num <= mid:
-                    count+=1
-            if count > mid:
-                r = mid
-            else:
-                l = mid + 1
-        return l
+        d = {}
+        for i in nums:
+            if i in d:
+                return i
+            d[i] = 1
