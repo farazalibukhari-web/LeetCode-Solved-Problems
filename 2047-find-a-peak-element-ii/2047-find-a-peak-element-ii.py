91f77641a642445487afdc5408e17a06f8c350cb
Faraz Ali Bukhari Syed <farazsyed3311@gmail.com>
2025-11-02 12:06:51 +0100

    Time: 0 ms (100%), Space: 32.5 MB (53.81%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.py
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.py
@@ -1,16 +1,33 @@
 class Solution(object):
-    def findPeakGrid(self, mat):
-        """
-        :type mat: List[List[int]]
-        :rtype: List[int]
-        """
-        max_num=0
-        for row in range(len(mat)):
-            num=max(mat[row])
+    def findPeakGrid(self,mat):
+        row, col = len(mat), len(mat[0])
+        left, right = 0, col - 1
+
+        while left <= right:
+            mid_col = (left + right) // 2
+
+            # Find the row index of the maximum element in mid_col
+            max_row = 0
+            for row_num in range(row):
+                if mat[row_num][mid_col] > mat[max_row][mid_col]:
+                    max_row = row_num
             
-            if max_num<num:
-                max_num=max(num,max_num)
-                max_num_index = mat[row].index(num)
-                row1=row
-        
-        return [row1,max_num_index]
+            curr_val  = mat[max_row][mid_col]
+            if mid_col - 1 >= 0:
+                left_val  = mat[max_row][mid_col - 1] 
+            else:
+                left_val  = -1
+            if mid_col + 1 < col:
+                right_val = mat[max_row][mid_col + 1] 
+            else:
+                right_val = -1
+            
+            # Compare with left and right neighbors
+            if curr_val > left_val and curr_val > right_val:
+                return [max_row, mid_col]
+            elif right_val > curr_val:
+                left = mid_col + 1
+            else:
+                right = mid_col - 1
+
+        return [-1, -1]
