12f1e00769d83e81c1f95d899371e855071ef7db
Faraz Ali Bukhari Syed <farazsyed3311@gmail.com>
2025-11-18 22:33:26 +0100

    Time: 35 ms (81.4%), Space: 13.5 MB (77.89%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.py
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.py
@@ -7,6 +7,5 @@ class Solution(object):
             else:
                 st.append(i)
         return "".join(st)
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 
         
